a25cf9f79904faec0dd12064a1028104a0d15dc9
TJ-Blinn <tjblinn@hotmail.com>
2021-07-30 03:53:01 +0000

    return an object containing counts of everything that the input object listed

---
+++ countOnly.js
@@ -0,0 +1,50 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual}  ===  ${expected} .`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected} .`);
+  }
+};
+
+// Test Code
+// assertEqual("Lighthouse Labs", "Lighthouse Labs");
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(1, 2);
+
+/* countOnly // take in a collection of items and return counts for a specific subset
+countOnly will be given an array and an object. It will return an object containing
+counts of everything that the input object listed.
+* capture onyl keys with trythy value
+* exclude strings not captured in object
+* exclude strings that are not also in the array
+=> report back how many instances of each string were found in the
+allItems array of strings
+*/
+
+const countOnly = function (allItems, itemsToCount) {
+  const results = {};
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+    // console.log(item);
+  }
+  //console.log("---------", results);
+  return results;
+};
+
+const firstNames = ["Karl", "Salima", "Agouhanna", "Fang", "Kavith", "Jason", "Salima", "Fang", "Joe"];
+
+const result1 = countOnly(firstNames, { Jason: true, Karima: true, Fang: true, Agouhanna: false });
+
+// *** test cases
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
