8d3598834b5d17d08d65963165c2cd01c2115b39
TJ-Blinn <tjblinn@hotmail.com>
2021-08-12 19:45:41 +0000

    refactored with modules

--- middle.js
+++ middle.js
@@ -1,25 +1,25 @@
-const eqArrays = function (arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-  // if array length 1 is NOT equal ( === ) to array length 2, return false
-};
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed.`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed.`);
-  }
-};
-// assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
+// const eqArrays = function (arr1, arr2) {
+//   if (arr1.length === arr2.length) {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//   return false;
+//   // if array length 1 is NOT equal ( === ) to array length 2, return false
+// };
+
+// const assertArraysEqual = function (actual, expected) {
+//   if (eqArrays(actual, expected)) {
+//     console.log(`✅✅✅ Assertion Passed.`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed.`);
+//   }
+// };
 
 // ************** Middle function take in an array and return the middle-most element(s) of the given array.
 const middle = function (array) {
@@ -38,17 +38,20 @@ const middle = function (array) {
     return midValue;
   }
 };
+module.exports = middle;
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// TEST CODE assertArraysEqual(middle
 
-// TEST Code
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 
-//middle([1]) // => []
+// TEST Code middle
+
+// middle([1]); // => []
 //middle([1, 2]) // => []
 //middle([1, 2, 3]) // => [2]
 //middle([1, 2, 3, 4, 5]) // => [3]
